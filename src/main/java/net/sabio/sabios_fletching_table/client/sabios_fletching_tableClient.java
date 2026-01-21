8bc4239d03555f49c26deb2b8e96a0a35258c090
Sabio <gamester.sad@gmail.com>
2026-01-21 21:31:06 +0000

    add fletching table screen (which is buggy and not usable, like at all)

--- src/main/java/net/sabio/sabios_fletching_table/client/sabios_fletching_tableClient.java
+++ src/main/java/net/sabio/sabios_fletching_table/client/sabios_fletching_tableClient.java
@@ -1,10 +1,14 @@
 package net.sabio.sabios_fletching_table.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import net.sabio.sabios_fletching_table.sabios_fletching_table;
+import net.sabio.sabios_fletching_table.screen.FletchingTableScreen;
 
 public class sabios_fletching_tableClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        HandledScreens.register(sabios_fletching_table.FLETCHING_TABLE_SCREEN_HANDLER, FletchingTableScreen::new);
     }
 }
--- src/main/java/net/sabio/sabios_fletching_table/screen/FletchingTableScreen.java
+++ src/main/java/net/sabio/sabios_fletching_table/screen/FletchingTableScreen.java
@@ -1,4 +1,34 @@
 package net.sabio.sabios_fletching_table.screen;
 
-public class FletchingTableScreen {
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class FletchingTableScreen extends HandledScreen<FletchingTableScreenHandler> {
+    private static final Identifier TEXTURE = Identifier.of("minecraft", "textures/gui/container/generic_54.png");
+
+    public FletchingTableScreen(FletchingTableScreenHandler handler, PlayerInventory playerInventory, Text title) {
+        super(handler, playerInventory, title);
+        this.backgroundWidth = 176;
+        this.backgroundHeight = 166;
+    }
+
+    @Override
+    protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
+        int x = (width - backgroundWidth) / 2;
+        int y = (height - backgroundHeight) / 2;
+        context.drawTexturedQuad(TEXTURE, x, y, x + backgroundWidth, y + backgroundHeight, 0.0f, 176.0f / 256.0f, 0.0f, 166.0f / 256.0f);
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        super.renderBackground(context, mouseX, mouseY, delta);
+        super.render(context, mouseX, mouseY, delta);
+        drawMouseoverTooltip(context, mouseX, mouseY);
+    }
 }
